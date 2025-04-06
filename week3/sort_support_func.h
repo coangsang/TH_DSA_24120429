ba81f294f60b2bed63c25416f18a5dcbe3df9cd5
Thuan <24120459@gmail.student.hcmus.edu.vn>
2025-04-06 11:49:37 +0700

    helper functions for sort algorithms

---
+++ week3/sort_support_func.h
@@ -0,0 +1,5 @@
+void swap(int &a, int &b);
+void merge(int nums[], int left, int mid, int right);
+void merge_sort_recursive(int nums[], int left, int right);
+void quick_sort_recursive(int nums[], int left, int right);
+void counting_sort_for_radix(int nums[], int n, int exp, int base);

f9abcb4ba3cbf817ffb07553bae805a967728680
Thuan <24120459@gmail.student.hcmus.edu.vn>
2025-04-08 23:05:12 +0700

    feat: output to file

--- week3/test.cpp
+++ week3/test.cpp
@@ -1,35 +1,47 @@
 #include <bits/stdc++.h>
 #include "sort_algo.h"
-#include "DataGenerator.cpp"
+#include "DataGenerator.h"
 using namespace std;
 
 
 int main(){
-    int dataSize[] = {1000, 10000, 100000, 1000000};
-    // 0: random data
-    // 1: sorted data
-    // 2: reverse data
-    // 3: nearly sorted data
-    int dataOrder[] = {0, 1, 2, 3};
-
+    int dataSize[] = {10000,30000,50000,100000};
+    int dataOrder[] = {0,1,2,3};
+    char* dataOrderName[] = {"Random", "Sorted", "Reverse Sorted", "Almost Sorted"};
     int nOfDataSize = sizeof(dataSize)/sizeof(dataSize[0]);
     int nOfDataOrder = sizeof(dataOrder)/sizeof(dataOrder[0]);
-    int nOfSort = sizeof(typeOfSort)/sizeof(typeOfSort[0]);
+    
+    FILE* statisticFile = fopen("statistic.txt","w");
+    if(statisticFile == NULL){
+        cout<<"Error opening file"<<endl;
+        return 1;
+    }
 
     for(int i = 0; i < nOfDataOrder; i++){
+        fprintf(statisticFile,"Data Order: %s\n",dataOrderName[i]);
+        fprintf(statisticFile,"Data Size-Sort Type-Time Taken-Number of Comparisons\n");
         for(int j = 0; j < nOfDataSize;j++){
             int n = dataSize[j];
             int* nums = new int[n];
             GenerateData(nums, n, dataOrder[i]);
+            int* numsCopy = new int[n];
             for(int k = 0; k < nOfSort; k++){
+                
+                for(int l = 0; l < n; l++){
+                    numsCopy[l] = nums[l];
+                }
+                
                 clock_t start = clock();
-                typeOfSort[k](nums, n);
+                typeOfSort[k](numsCopy, n);
                 clock_t end = clock();
                 double timeTaken = double(end - start) / CLOCKS_PER_SEC;
-                cout << "Data size: " << n << ", Data order: " << dataOrder[i] << ", Sort type: " << typeOfSortName[k] << ", Time taken: " << timeTaken << " seconds" << endl;
+                fprintf(statisticFile,"%d-%s-%f-%d\n",n,typeOfSortName[k],timeTaken,0);
             }
+            delete[] numsCopy;
             delete[] nums;
         }
+        fprintf(statisticFile,"\n");
     }
+    fclose(statisticFile);
     return 0;
 }

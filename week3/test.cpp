8dff68558b9766ab411b46f07a7eb5534a344ae5
Thuan <24120459@gmail.student.hcmus.edu.vn>
2025-04-11 10:09:54 +0700

    fix print to file data type of count compare

--- week3/test.cpp
+++ week3/test.cpp
@@ -36,7 +36,7 @@ int main(){
                 clock_t end = clock();
                 double timeTaken = (double)(end - start) / CLOCKS_PER_SEC;
 
-                fprintf(statisticFile,"%d-%s-%f-%d\n",n,typeOfSortName[k],timeTaken,0);
+                fprintf(statisticFile,"%d-%s-%f-%llu\n",n,typeOfSortName[k],timeTaken,0);
             }
             delete[] numsCopy;
             delete[] nums;
